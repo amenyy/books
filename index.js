36877d87c910ac64a27f42ecb87bd095ab4e53cd
MeowDevelopment <ameny.ferchichi99@gmail.com>
2022-11-16 09:58:59 +0100

    fix errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./router");
 const mongoose = require("mongoose");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
